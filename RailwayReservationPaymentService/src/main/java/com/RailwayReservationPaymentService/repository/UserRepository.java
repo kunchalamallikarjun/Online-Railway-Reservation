5a474d085809045341baae991a4fc4d0698cba05
kunchalamallikarjun <kunchalamallikarjun4@gmail.com>
2022-01-06 00:34:38 +0530

    Backend end commit

---
+++ RailwayReservationPaymentService/src/main/java/com/RailwayReservationPaymentService/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.RailwayReservationPaymentService.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import com.RailwayReservationPaymentService.model.UserDetailsData;
+
+/*
+ Repository For Connecting MongoDb With UserDetails Model
+ */
+@Repository
+public interface UserRepository extends MongoRepository<UserDetailsData, Integer> {
+
+}
